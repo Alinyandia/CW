7346b1b18f49fa13d523699bef357bd46de92b11
Alinyandia <arshaymardanova@mail.ru>
2017-02-08 14:35:39 +0300

    Add files via upload

---
+++ qwerty.py
@@ -0,0 +1,39 @@
+import json
+import os
+
+def eroffn():
+    file = open("result.txt", 'w', encoding ='utf-8')
+    for (path, dirs, files) in os.walk("/Users/alinashaymardanova/Downloads/tat_vk_corpus"):
+        i = 1
+        while i < len(files):
+            auth = []
+            d = {}
+            f = open(''.join(path) + '/' + ''.join(files[i]), 'r', encoding = 'utf-8')
+            f1 = f.read()
+            res = json.loads(f1)
+            for key in res:
+                if key != "0":
+                    members_count = res[key]['members_count']
+                    name = res[key]['name']
+                    for num in res[key]['posts']: #идем по номерам записей
+                        if res[key]['posts'][num]['comments']: #если есть комменты, то смотрим их авторов
+                            for c_num in res[key]['posts'][num]['comments']:
+                                auth.append(res[key]['posts'][num]['comments'][c_num]['author'])
+                        else:
+                            auth.append(res[key]['posts'][num]['author']) #достаем инфу об авторе
+            file.write('Имя группы' + ' ' + str(name) + '\n' + 'Количество человек' + ' ' + str(members_count) + '\n' + 'Авторы:' + '\n')
+            for memb in auth: #формируем словарь частотности каждого пользователя
+                if 'id' in memb:
+                    if memb['id'] in d:
+                        d[memb['id']] += 1
+                    else:
+                        d[memb['id']] = 1
+                file.write( ''.join(str(memb)) + '\n')
+            file.write(''.join(str(d)) + '\n' + '\n')               
+            f.close()
+            i += 1
+    file.close()
+def main():
+    a = eroffn()
+if __name__== '__main__':
+    main()
