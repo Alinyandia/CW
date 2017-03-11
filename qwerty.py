95b5c4ddb8d96f69c7a09ad5648d523ef4495cb8
Alinyandia <arshaymardanova@mail.ru>
2017-03-11 17:15:07 +0300

    Update qwerty.py

--- qwerty.py
+++ qwerty.py
@@ -1,15 +1,23 @@
 import json
 import os
 
+
 def inform(res, file):
     for key in res:
         if key != "0":
-            members_count = res[key]['members_count']
-            name = res[key]['name']
+            members_count = res[key]['members_count'] #количество подписчиков
+            name = res[key]['name'] #название сообщества
             for num in res[key]['posts']: #идем по номерам записей
                 if res[key]['posts'][num]['comments']: #если есть комменты, то смотрим их авторов
+                    auth.append("Комментарии к записи номер"+ " " + num)
+                    if 'author' in res[key]['posts'][num]: #если есть строка с автором записи, то записываем инф-ию о нём
+                        auth.append("Автор записи")
+                        auth.append(res[key]['posts'][num]['author'])
+                    else:
+                        auth.append("Автор удалён")
                     for c_num in res[key]['posts'][num]['comments']:
                         auth.append(res[key]['posts'][num]['comments'][c_num]['author'])
+                    auth.append("Конец комментариев")
                 else:
                     auth.append(res[key]['posts'][num]['author']) #достаем инфу об авторе
     file.write('Имя группы' + ' ' + str(name) + '\n' + 'Количество человек' + ' ' + str(members_count) + '\n' + 'Авторы:' + '\n')
@@ -24,7 +32,8 @@ def di(auth):
                 d[memb['id']] = 1
         file.write( ''.join(str(memb)) + '\n')
     file.write(''.join(str(d)) + '\n' + '\n')
-    
+    f.close()
+
 
 file = open("result.txt", 'w', encoding ='utf-8')
 for (path, dirs, files) in os.walk("/Users/alinashaymardanova/Downloads/tat_vk_corpus"):
